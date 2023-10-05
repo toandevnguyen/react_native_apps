c98af158e106cbe0593df99531b4b44b5bf4f209
Your Name.com <toandevnguyen@gmail.com>
2023-10-05 11:06:11 +0700

    first commit

---
+++ src/screens/Todos.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {ScrollView, Text} from 'react-native';
+
+import firestore from '@react-native-firebase/firestore';
+import {Appbar, TextInput, Button} from 'react-native-paper';
+function Todos() {
+  const ref = firestore().collection('todos');
+  return (
+    <>
+      <Appbar>
+        <Appbar.Content title={'TODOs List'} />
+      </Appbar>
+      <ScrollView style={{flex: 1}}>
+        <Text>List of TODOs!</Text>
+      </ScrollView>
+      <TextInput label={'New Todo'} onChangeText={() => {}} />
+      <Button onPress={() => {}}>Add TODO</Button>
+    </>
+  );
+}
+
+export default Todos;
